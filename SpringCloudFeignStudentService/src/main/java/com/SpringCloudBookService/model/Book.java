4517c3d8acd89052b8b830389ffee640ae024031
Umesh Annegirikar <annegirikar.umesh@gmail.com>
2025-02-07 21:00:02 +0530

    Add files via upload

---
+++ SpringCloudFeignStudentService/src/main/java/com/SpringCloudBookService/model/Book.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.SpringCloudBookService.model;
+
+/**
+ * @author a631954
+ *
+ */
+public class Book 
+{
+	private Integer bookId;
+    private String bookName;
+    private Double bookCost;
+    
+	public Book(Integer bookId, String bookName, Double bookCost) {
+		super();
+		this.bookId = bookId;
+		this.bookName = bookName;
+		this.bookCost = bookCost;
+	}
+
+	public Integer getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(Integer bookId) {
+		this.bookId = bookId;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public Double getBookCost() {
+		return bookCost;
+	}
+
+	public void setBookCost(Double bookCost) {
+		this.bookCost = bookCost;
+	}
+   
+    
+}
